eed5efc9a2c00abf4a51b6dd881bc75add004f34
NguyenDacHoangAnh <anh.ndh204510@sis.hust.edu.vn>
2023-01-01 15:18:12 +0700

    bar/line/pie/geo chart

--- src/App.js
+++ src/App.js
@@ -9,11 +9,11 @@ import Team from "./scenes/team";
 import Contacts from "./scenes/contacts";
 import Invoices from "./scenes/invoices";
 import Form from "./scenes/form";
-// import Bar from "./scenes/bar";
-// import Line from "./scenes/line";
-// import Pie from "./scenes/pie";
+import Bar from "./scenes/bar";
+import Line from "./scenes/line";
+import Pie from "./scenes/pie";
 import FAQ from "./scenes/faq";
-// import Geography from "./scenes/geography";
+import Geography from "./scenes/geography";
 import Calendar from "./scenes/calendar";
 
 function App() {
@@ -35,11 +35,11 @@ function App() {
               <Route path="/contacts" element={<Contacts />}></Route>
               <Route path="/invoices" element={<Invoices />}></Route>
               <Route path="/form" element={<Form />}></Route>
-              {/* <Route path="/bar" element={<Bar />}></Route> */}
-              {/* <Route path="/line" element={<Line />}></Route> */}
-              {/* <Route path="/pie" element={<Pie />}></Route> */}
+              <Route path="/bar" element={<Bar />}></Route>
+              <Route path="/line" element={<Line />}></Route>
+              <Route path="/pie" element={<Pie />}></Route>
               <Route path="/faq" element={<FAQ />}></Route>
-              {/* <Route path="/geography" element={<Geography />}></Route> */}
+              <Route path="/geography" element={<Geography />}></Route>
               <Route path="/calendar" element={<Calendar />}></Route>
             </Routes>
           </main>
